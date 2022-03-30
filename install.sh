ece7ba9478242c5e9449d5b627a6a5a27f84b5b7
papersnake <prnake@gmail.com>
2022-03-30 20:34:48 +0800

    config install

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo apt update
 sudo apt install zsh subversion neofetch vim git prettyping -y
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $(whoami)
+printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg
