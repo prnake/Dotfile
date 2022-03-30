73c9b4af703c379d1b87eb5a067e88918bd73921
papersnake <prnake@gmail.com>
2022-03-30 20:47:28 +0800

    add gitpod install

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 sudo apt update
 sudo apt install zsh subversion neofetch vim git prettyping -y
 sudo chsh -s $(which zsh) $(whoami)
+cp .zshrc ~/
+cp .tmux.conf ~/
+cp .*.zsh ~/
 printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg
