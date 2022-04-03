8dab4f7afd385ecb03e4985d88e71e8d0cd591fc
Papersnake <prnake@gmail.com>
2022-04-03 17:41:28 +0000

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname "$0")
 sudo apt update
 sudo apt install zsh subversion neofetch vim git prettyping asciinema tmux -y
-cargo install exa
-sudo chsh -s $(which zsh) $(whoami)
+brew install exa
+cp $DIR/.zshrc ~
+cp $DIR/.gitconfig ~/
+cp $DIR/.*.zsh ~/
+cp $DIR/.*.conf ~/
 printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg
+sudo chsh -s $(which zsh) $(whoami) 
 zsh
