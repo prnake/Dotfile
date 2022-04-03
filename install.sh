03299ef115d22d25423a261306202124ab1fcec9
prnake <prnake@gmail.com>
2022-04-03 17:10:44 +0000

    change to cargo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install zsh subversion neofetch vim git prettyping asciinema tmux -y
-nix-env -i exa
+cargo install exa
 sudo chsh -s $(which zsh) $(whoami)
 printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg
 zsh
