1d23d75cf466f64d2940430a4f307d6954619e21
prnake <prnake@gmail.com>
2022-04-03 16:56:24 +0000

    exa

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 sudo apt update
-sudo apt install zsh subversion neofetch vim git prettyping exa asciinema tmux -y
+sudo apt install zsh subversion neofetch vim git prettyping asciinema tmux -y
+cargo install exa
 sudo chsh -s $(which zsh) $(whoami)
 cp .zshrc ~/
 cp .tmux.conf ~/
 cp .*.zsh ~/
 printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg
+zsh
