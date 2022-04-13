ddee7c0a36659a25badacb614cdf69b4ff3540bc
papersnake <prnake@gmail.com>
2022-04-13 13:23:02 +0000

    add ssh

--- install.sh
+++ install.sh
@@ -8,6 +8,13 @@ cp $DIR/.zshrc ~
 cp $DIR/.gitconfig ~/
 cp $DIR/.*.zsh ~/
 cp $DIR/.*.conf ~/
-printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg
+
+printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.
+mkdir -p ~/.ssh &&
+      echo $SSH_PUBLIC_KEY > ~/.ssh/id_ed25519.pub &&
+      chmod 644 ~/.ssh/id_ed25519.pub &&
+      echo $SSH_PRIVATE_KEY > ~/.ssh/id_ed25519 &&
+      chmod 600 ~/.ssh/id_ed25519
+
 sudo chsh -s $(which zsh) $(whoami) 
 zsh
