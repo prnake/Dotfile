26bb60020d85c1b76e17dc6a2a5e127427e820c5
prnake <prnake@gmail.com>
2022-04-24 15:45:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ cp $DIR/.*.conf ~/
 
 printf "\n[settings]\napi_key = $WAKA_TIME_API_KEY\n" > ~/.wakatime.cfg
 mkdir -p ~/.ssh &&
-      echo $SSH_PUBLIC_KEY > ~/.ssh/id_ed25519.pub &&
+      echo -e $SSH_PUBLIC_KEY > ~/.ssh/id_ed25519.pub &&
       chmod 644 ~/.ssh/id_ed25519.pub &&
-      echo $SSH_PRIVATE_KEY > ~/.ssh/id_ed25519 &&
+      echo -e $SSH_PRIVATE_KEY > ~/.ssh/id_ed25519 &&
       chmod 600 ~/.ssh/id_ed25519
 
 sudo chsh -s $(which zsh) $(whoami) 
